a8d0254649b9f13d78466c059f1e4535a7dfe164
fsjlww <fsjlww@163.com>
2025-08-07 01:49:29 +0800

    config.rs
    自己服务器IP,key

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["202.110.24.5"];
+pub const RS_PUB_KEY: &str = "m30tx8IrsStIqHzmfUVqktV8cjcC30n1kB8E3aOq3cs=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
